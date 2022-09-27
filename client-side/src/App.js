65a93d4b1fc8172289f42a7dabc64c9aad9bfc9c
Milton Vandol Goh <100271526+Vandal19@users.noreply.github.com>
2022-09-27 15:45:17 -0700

    set up theme for coinbarter

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,35 +1,54 @@
-import './App.css';
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import ResponsiveAppBar from './components/NavBar';
-import Products from './components/products';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import ResponsiveAppBar from "./components/NavBar";
+import Products from "./components/products";
+import { Button, Container } from "@mui/material";
+import { ThemeProvider } from "@mui/system";
+import theme from "./styles/theme"
 
 function App() {
-  const [name, setName] = useState("")
-  const [home, setHome] =useState("")
+  const [name, setName] = useState("");
+  const [home, setHome] = useState("");
 
   useEffect(() => {
-    axios.get("http://localhost:8000/home")
-    .then(res => {setHome(res.data)})
-  },[])
+    axios.get("http://localhost:8000/home").then((res) => {
+      setHome(res.data);
+    });
+  }, []);
 
   async function postName(e) {
-    e.preventDefault()
+    e.preventDefault();
 
     try {
       await axios.post("http://localhost:8000/post_name", {
-        name
-      })
+        name,
+      });
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   }
 
   return (
-    <div className="App">
-     <ResponsiveAppBar />
-     {/* <Products /> */}
-    </div>
+    <ThemeProvider theme={theme}>
+
+    <Container
+      maxWidth="xl"
+      sx={{
+        background: "#fff",
+      }}
+      >
+      {/*
+      ResponsiveAppBar
+      Banner
+      Title
+      Products
+      Footer
+      Searchbox
+    */}
+      <ResponsiveAppBar />
+    </Container>
+    </ThemeProvider>
   );
 }
 
