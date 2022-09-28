f86d173a6c98ad776d7ba1c12fadac2dfb31570f
Winston Limanto <wmlimanto@gmail.com>
2022-09-27 17:54:26 -0700

    add styling for navbar

---
+++ client-side/src/styles/navbar/index.js
@@ -0,0 +1,24 @@
+import { Typography } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import { Box, color } from "@mui/system";
+import { Colors } from "../theme";
+
+// container
+export const NavBarContainer = styled(Box)(() => ({
+
+  display: 'flex',
+  marginTop: 4,
+  justifyContent: 'center',
+  alignItems: 'center',
+  padding: '2px 8px'
+}));
+
+// header
+export const NavBarHeader = styled(Typography)(() => ({
+
+  padding: '4px',
+  flexGrow: 1,
+  fontSize: '4em',
+  fontFamily: '"Fira Code", monospace',
+  color: Colors.primary
+}));
