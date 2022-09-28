4eaf51f1567d71be6a49ab0855af02b800fd3b12
Winston Limanto <wmlimanto@gmail.com>
2022-09-27 19:55:29 -0700

    add styles for list items and icon actions for both desktop and mobile

--- client-side/src/styles/navbar/index.js
+++ client-side/src/styles/navbar/index.js
@@ -1,6 +1,6 @@
-import { Typography } from "@mui/material";
+import { List, Typography } from "@mui/material";
 import { styled } from "@mui/material/styles";
-import { Box, color } from "@mui/system";
+import { Box } from "@mui/system";
 import { Colors } from "../theme";
 import "@fontsource/fira-code";
 
@@ -22,4 +22,32 @@ export const NavBarHeader = styled(Typography)(() => ({
   fontSize: '4em',
   fontFamily: '"Fira Code", monospace',
   color: Colors.primary
+}));
+
+// my list of items on navbar
+export const MyList = styled(List)(({ type }) => ({
+
+  display: type === 'row' ? 'flex' : 'block',
+  flexGrow: 3,
+  justifyContent: 'center',
+  alignItems: 'center'
+}));
+
+// icon actions for mobile
+export const ActionIconsContainerMobile = styled(Box)(() => ({
+  display: 'flex',
+  background: Colors.shaft,
+  position: 'fixed',
+  bottom: 0,
+  left: 0,
+  width: '100%',
+  alignItems: 'center',
+  zIndex: 99,
+  borderTop: `1px solid ${Colors.border}`
+}));
+
+// icon actions for desktop
+export const ActionIconsContainerDesktop = styled(Box)(() => ({
+
+  flexGrow: 0
 }));
