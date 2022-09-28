10fdd1ddb3efb9d1259a466f0801aa4dfbccad0c
Winston Limanto <wmlimanto@gmail.com>
2022-09-27 19:52:15 -0700

    update navbar for desktop

--- client-side/src/components/navbar/NavBarDesktop.js
+++ client-side/src/components/navbar/NavBarDesktop.js
@@ -1,4 +1,13 @@
-import { NavBarContainer, NavBarHeader } from "../../styles/navbar";
+import { ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
+import { 
+  ActionIconsContainerDesktop, 
+  ActionIconsContainerMobile, 
+  MyList, 
+  NavBarContainer, 
+  NavBarHeader 
+} from "../../styles/navbar";
+import Actions from "./actions";
+import SearchIcon from '@mui/icons-material/Search';
 
 
 export default function NavBarDesktop({ matches }) {
@@ -6,6 +15,18 @@ export default function NavBarDesktop({ matches }) {
   return (
     <NavBarContainer>
       <NavBarHeader>CoinBarter</NavBarHeader>
+      <MyList type="row">
+        <ListItemText primary="Cell Phones" />
+        <ListItemText primary="Computers" />
+        <ListItemText primary="Gaming" />
+        <ListItemText primary="Television" />
+        <ListItemButton>
+          <ListItemIcon>
+            <SearchIcon />
+          </ListItemIcon>
+        </ListItemButton>
+      </MyList>
+      <Actions matches={matches}/>
     </NavBarContainer>
   );
   
