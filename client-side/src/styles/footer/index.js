3af958c10f06ff27dce9793956cace92fb5cc595
Milton Vandol Goh <100271526+Vandal19@users.noreply.github.com>
2022-09-28 21:09:00 -0700

    Complete footer page

--- client-side/src/styles/footer/index.js
+++ client-side/src/styles/footer/index.js
@@ -1,4 +1,4 @@
-import { Typography, Box } from '@mui/material'
+import { Typography, Box, TextField } from '@mui/material'
 import { styled } from "@mui/system"
 import React from 'react'
 import { Colors } from '../theme';
@@ -12,15 +12,28 @@ export const FooterContainer = styled(Box)(({theme}) => ({
   background: Colors.shaft,
   color: Colors.white,
   padding: 4,
-  paddingTop: 48,
+  paddingTop: 36,
   paddingBottom: 48,
   fontSize: 12,
   [theme.breakpoints.up("md")]: {
     padding: 40,
-    fontSize: 14
+    fontSize: 14,
+    position: 'relative'
   }
 }))
 
+export const SubscribeTextField = styled(TextField)(() => ({
+  '.MuiInputLabel-root': {
+    color: Colors.secondary,
+  },
+  '.MuiInput-root::before': {
+    borderBottom: `1px solid`,
+    color: Colors.secondary
+  },
+  '.MuiInputBase-input': {
+    color: Colors.white
+  }
+}))
 
 
 
