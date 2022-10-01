b34dadcebb0f5362027eb8ce792d56ac02646c22
Milton Vandol Goh <100271526+Vandal19@users.noreply.github.com>
2022-09-30 21:31:39 -0700

    create asyncthunk

--- client-side/src/features/productSlice.js
+++ client-side/src/features/productSlice.js
@@ -1,14 +1,35 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
   items: [],
   status: null
 }
 
+export const productsFetch = createAsyncThunk(
+  "products/productsFetch",
+  async () => {
+    const response = await axios.get("http://localhost:8000/products")
+    return response?.data
+  }
+)
+
 const productsSlice = createSlice({
   name: 'products',
   initialState,
   reducers: {},
+  extraReducers: {
+    [productsFetch.pending]: (state, action) => {
+      state.status = "pending"
+    },
+    [productsFetch.fulfilled]: (state, action) => {
+      state.status = "success"
+      state.items = action.payload
+    },
+    [productsFetch.rejected]: (state, action) => {
+      state.status = "rejected"
+    }
+  }
 })
 
 export default productsSlice.reducer
