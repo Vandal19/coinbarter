fc996572f85e46b70920868fe04c46b281ccc402
Milton Vandol Goh <100271526+Vandal19@users.noreply.github.com>
2022-09-30 21:40:09 -0700

    call data from backend

--- client-side/src/index.js
+++ client-side/src/index.js
@@ -5,13 +5,15 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
-import productsReducer from "./features/productSlice";
+import productsReducer, { productsFetch } from "./features/productSlice";
 
 const store = configureStore({
   reducer: {
     products: productsReducer
   },
 });
+
+store.dispatch(productsFetch())
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
