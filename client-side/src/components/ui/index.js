367922323f86a6d3a8f5720aff304548348ef4ab
Winston Limanto <wmlimanto@gmail.com>
2022-10-04 18:53:35 -0700

    add ui component for quantity of product in product details dialog

---
+++ client-side/src/components/ui/index.js
@@ -0,0 +1,46 @@
+import { IconButton, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import { useState } from "react";
+import { Colors } from "../../styles/theme";
+import { productDetailQuantity } from "./productDetailQuantity";
+import RemoveIcon from '@mui/icons-material/Remove';
+import AddIcon from '@mui/icons-material/Add';
+
+
+
+export default function ProductIncDec() {
+
+  const productDetailQuantityValue = productDetailQuantity(1, 20);
+  // set value
+  const [value, setValue] = useState(1);
+
+  return (
+    <Box display="flex">
+      <IconButton
+        sx={{
+          borderRadius: 0,
+          background: `${Colors.secondary}`
+        }}
+        onClick={() => setValue(productDetailQuantityValue(value - 1))}>
+        <RemoveIcon />
+      </ IconButton>
+
+      <Typography
+        variant="h6"
+        sx={{
+          border: `1px solid ${Colors.secondary}`,
+          p:2
+        }}
+      > {value} </Typography>
+
+      <IconButton
+        sx={{
+          borderRadius: 0,
+          background: `${Colors.secondary}`
+        }}
+        onClick={() => setValue(productDetailQuantityValue(value + 1))}>
+        <AddIcon />
+      </ IconButton>
+    </Box>
+  )
+}
