4cff6b43a66d030097dc2f5e2b5fd69363152bf7
Milton Vandol Goh <100271526+Vandal19@users.noreply.github.com>
2022-10-05 16:49:48 -0700

    Test backend login auth using Postman

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -9,23 +9,37 @@ const getUserByEmail = require("../db/database");
 router.post("/login", async (req, res) => {
   validateForm(req, res);
 
-  if (getUserByEmail.rowCount > 0) {
-    const samePw = await bcrypt.compare(
-      req.body.password,
-      getUserByEmail.rows[0].password
-    );
-    if (samePw) {
-      req.session.user = {
-        email: req.body.email,
-        id: getUserByEmail.rows[0].id,
-      };
-      res.json({ loggedIn: true, email });
+  const user = await getUserByEmail(req.body.email)
+
+  if (user) {
+    const inputPw = req.body.password
+    if (inputPw === user.password) {
+      res.send(user);
     } else {
-      res.json({ loggedIn: false, status: "Wrong email or password" });
+      res.sendStatus(403);
     }
   } else {
-    res.json({ loggedIn: false, status: "Wrong email or password" });
+    res.sendStatus(403);
   }
+
+  /* For Bcrypt authentication */
+  // if (getUserByEmail.rowCount > 0) {
+  //   const samePw = await bcrypt.compare(
+  //     req.body.password,
+  //     getUserByEmail.rows[0].password
+  //   );
+  //   if (samePw) {
+  //     req.session.user = {
+  //       email: req.body.email,
+  //       id: getUserByEmail.rows[0].id,
+  //     };
+  //     res.json({ loggedIn: true, email });
+  //   } else {
+  //     res.json({ loggedIn: false, status: "Wrong email or password" });
+  //   }
+  // } else {
+  //   res.json({ loggedIn: false, status: "Wrong email or password" });
+  // }
 });
 
 router.post("/signup", async (req, res) => {});
