baf750c27bb6cefe3797e860db4d31357d69b5d3
Winston Limanto <wmlimanto@gmail.com>
2022-10-05 22:27:25 -0700

    import ItemsInCart fn to render cart items in checkout page + edit styling

--- client-side/src/components/checkout/Checkout.js
+++ client-side/src/components/checkout/Checkout.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import { useState } from 'react';
-import { Container, Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@mui/material';
+import { useState, useEffect } from 'react';
+import { Grid, Container, Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@mui/material';
 import AddressForm from './AddressForm';
 import PaymentForm from './PaymentForm';
+
+import ItemsInCart from '../cart/cartItems';
 
 
 // manage steps for checkout
@@ -13,6 +15,17 @@ export default function Checkout() {
 
   // states to move through steps for checkout
   const [activeStep, setActiveStep] = useState(0);
+
+  // hook to generate checkout token when user enters checkout process
+  // useEffect(() => {
+  //   const generateToken = async () => {
+  //     try {
+        
+  //     } catch (error) {
+        
+  //     }
+  //   }
+  // }, []);
 
   // confirmation msg
   const Confirmation = () => (
@@ -26,22 +39,28 @@ export default function Checkout() {
 
   return (
     <>
-      <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
-        <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
-          <Typography component="h1" variant="h4" align="center">
-            Checkout
-          </Typography>
-          <Stepper activeStep={activeStep} sx={{ pt: 3, pb: 5 }}>
-            {steps.map((step) => (
-              <Step key={step}>
-                <StepLabel>{step}</StepLabel>
-              </Step>
-            ))}
-          </Stepper>
-          {/* if we are on the last step, then show new component */}
-          {activeStep === steps.length ? <Confirmation /> : <Form />}
-        </Paper>
-      </Container>
+      <Grid container component="main" columns={16} spacing={3}
+            sx={{ mb: 4, height: '100vh' }}>
+        <Grid item xs={8}>
+          <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
+            <Typography component="h1" variant="h4" align="center">
+              Checkout
+            </Typography>
+            <Stepper activeStep={activeStep} sx={{ pt: 3, pb: 5 }}>
+              {steps.map((step) => (
+                <Step key={step}>
+                  <StepLabel>{step}</StepLabel>
+                </Step>
+              ))}
+            </Stepper>
+            {/* if we are on the last step, then show new component */}
+            {activeStep === steps.length ? <Confirmation /> : <Form />}
+          </Paper>
+        </Grid>
+        <Grid item xs={8}>
+          <ItemsInCart />
+        </Grid>
+      </Grid>
     </>
   );
 }
