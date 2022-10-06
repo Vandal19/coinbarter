c8fb46a236d45c6171c4e82c9fc5fb9d04a0edf9
Winston Limanto <wmlimanto@gmail.com>
2022-10-05 22:25:05 -0700

    add routes for checkout page and cart page

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -16,9 +16,11 @@ import { NavBarCategories } from "./styles/navbar";
 import MobileMenu from "./components/drawer";
 import { UIProvider } from "./context/ui";
 import Cart from "./components/cart";
+import ItemsInCart from "./components/cart/cartItems";
 import SearchBox from "./components/search";
 import LogIn from "./pages/Login";
 import Favorite from "./components/favorites"
+import Checkout from "./components/checkout/Checkout.js";
 
 
 function App() {
@@ -73,6 +75,17 @@ function App() {
             <Route path="/products/:id" element={<>
               <Products />
             </>} />
+
+            {/* Login Page*/}
+            <Route path="/checkout" element={<>
+            <Checkout />
+            </>} />
+
+            {/* FOR TESTING -- Cart Page*/}
+            <Route path="/cart" element={<>
+            <ItemsInCart />
+            </>} />
+
           </Routes>
           <Footer />
           <MobileMenu />
