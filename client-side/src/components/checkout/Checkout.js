b5f148229beb8bf6545e05ce2bd17c23ad07e5b7
Winston Limanto <wmlimanto@gmail.com>
2022-10-06 21:48:56 -0700

    manage states to move through shipping and payment steps of checkout

--- client-side/src/components/checkout/Checkout.js
+++ client-side/src/components/checkout/Checkout.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import { Grid, Container, Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@mui/material';
 import AddressForm from './AddressForm';
 import PaymentForm from './PaymentForm';
+import { useSelector, useDispatch } from "react-redux";
 
 import ItemsInCart from '../cart/cartItems';
 
@@ -14,7 +15,9 @@ const steps = ['Shipping address', 'Payment details'];
 export default function Checkout() {
 
   // states to move through steps for checkout
-  const [activeStep, setActiveStep] = useState(0);
+  const [activeStep, setActiveStep] = useState(1);
+  // states to capture shipping data
+  const [shippingData, setShippingData] = useState({});
 
   // hook to generate checkout token when user enters checkout process
   // useEffect(() => {
@@ -27,6 +30,24 @@ export default function Checkout() {
   //   }
   // }, []);
 
+  // capture items in cart data
+  const cart = useSelector((state) => state.cart);
+  // cart = {cartItems, cartTotalQuantity, cartTotalAmount}
+  const cartOrder = cart.cartTotalQuantity;
+  console.log("orders in cart:", cartOrder);
+
+
+  // move through checkout steps
+  const nextStep = () => setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  const backStep = () => setActiveStep((prevActiveStep) => prevActiveStep - 1);
+
+  // capture shipping data and move to payment step of checkout
+  const next = (data) => {
+    setShippingData(data);
+
+    nextStep();
+  }
+
   // confirmation msg
   const Confirmation = () => (
     <div>Confirmation</div>
@@ -34,13 +55,13 @@ export default function Checkout() {
 
   // render different components based on which step user is on
   const Form = () => activeStep === 0 
-    ? <AddressForm />
-    : <PaymentForm />
+    ? <AddressForm next={next} />
+    : <PaymentForm backStep={backStep} />
 
   return (
     <>
       <Grid container component="main" columns={16} spacing={3}
-            sx={{ mb: 4, height: '100vh' }}>
+            sx={{ mb: 10, height: '100vh' }}>
         <Grid item xs={8}>
           <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
             <Typography component="h1" variant="h4" align="center">
