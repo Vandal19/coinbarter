eb3129753e5e82008e694fe6aa9f8d6ad805eeed
Winston Limanto <wmlimanto@gmail.com>
2022-10-07 18:56:25 -0700

    import ethersjs and add fn to initialize connection with metamask wallet

--- client-side/src/components/checkout/PaymentForm.js
+++ client-side/src/components/checkout/PaymentForm.js
@@ -6,55 +6,74 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 import { Button } from '@mui/material';
 import { Box } from "@mui/system";
+import { useState } from "react";
+import { ethers } from "ethers";
+
+
+// initialize connection with metamask wallet
+const startPayment = async ({ setError, setTxs, ether, addr }) => {
+  try {
+    if (!window.ethereum)
+      throw new Error("No crypto wallet found. Please install it.");
+
+    await window.ethereum.send("eth_requestAccounts");
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    ethers.utils.getAddress(addr);
+    const tx = await signer.sendTransaction({
+      to: addr,
+      value: ethers.utils.parseEther(ether)
+    });
+    console.log({ ether, addr });
+    console.log("tx", tx);
+    setTxs([tx]);
+  } catch (err) {
+    setError(err.message);
+  }
+};
+
 
 export default function PaymentForm({backStep}) {
+
+  // states for error and capture tx
+  const [error, setError] = useState();
+  const [txs, setTxs] = useState([]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    setError();
+    await startPayment({
+      setError,
+      setTxs,
+      ether: data.get("ether"),
+      addr: data.get("addr")
+    });
+  };
+
   return (
     <>
       <Typography variant="h6" gutterBottom>
-        Payment method
+        Send ETH Payment
       </Typography>
-      <Grid container spacing={3}>
-        <Grid item xs={12} md={6}>
+      <Grid container spacing={3} component="form" onSubmit={handleSubmit}>
+        <Grid item xs={12}>
           <TextField
-            required
-            id="cardName"
-            label="Name on card"
+            id="addr"
+            label="Recipient Address"
             fullWidth
-            autoComplete="cc-name"
             variant="standard"
           />
         </Grid>
-        <Grid item xs={12} md={6}>
+        <Grid item xs={12}>
           <TextField
-            required
-            id="cardNumber"
-            label="Card number"
+            id="ether"
+            label="Amount in ETH"
             fullWidth
-            autoComplete="cc-number"
             variant="standard"
           />
         </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField
-            required
-            id="expDate"
-            label="Expiry date"
-            fullWidth
-            autoComplete="cc-exp"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField
-            required
-            id="cvv"
-            label="CVV"
-            helperText="Last three digits on signature strip"
-            fullWidth
-            autoComplete="cc-csc"
-            variant="standard"
-          />
-        </Grid>
+
         <br />
         <Grid item xs={12} sx={{ mt: 2 }}>
           <Box
