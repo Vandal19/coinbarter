f5300d4efd7b398e1c3eeb76213f65e70af0233f
Winston Limanto <wmlimanto@gmail.com>
2022-10-09 01:47:26 -0700

    edit component to render tx success msg with link to etherscan block explorer

--- client-side/src/components/checkout/TxList.js
+++ client-side/src/components/checkout/TxList.js
@@ -1,8 +1,12 @@
 import { Box } from "@mui/system";
 import Alert from '@mui/material/Alert';
 import Stack from '@mui/material/Stack';
+import { Button } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 // display tx hash to user once payment is fulfilled
+
+let txsURL = 'https://goerli.etherscan.io/tx/';
 
 export default function TxList({ txs }) {
   if (txs.length === 0) return null;
@@ -12,7 +16,16 @@ export default function TxList({ txs }) {
     <Stack sx={{ width: '100%' }}>
       {txs.map((item) => (
         <Box key={item}>
-          <Alert severity="success" alignItems="center">{item.hash}</Alert>
+          <Alert severity="success" alignItems="center">
+            Payment Complete: 
+            <a href={`https://goerli.etherscan.io/tx/${item.hash}`}
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+               View Transaction
+            </a>
+            {/* {item.hash} */}
+          </Alert>
         </Box>
       ))}
     </Stack>
