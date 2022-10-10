fbe9e7616d689dc4988eac45d8fa6798ec6693b7
Winston Limanto <wmlimanto@gmail.com>
2022-10-09 20:56:19 -0700

    call and display btc eth ltc price data from coingecko api

--- client-side/src/components/checkout/GetEthPrice.js
+++ client-side/src/components/checkout/GetEthPrice.js
@@ -1,8 +1,10 @@
-// GET PRICE OF ETHER IN USD
+// GET PRICE & PRICE CHANGE OF BTC ETH LTC IN USD
 
 import React from 'react'
 import { useState, useEffect } from 'react';
-import { Grid, Paper, Typography }from '@mui/material';
+import { Card, CardContent, CardMedia, Grid, Paper, Typography }from '@mui/material';
+import SouthEastIcon from '@mui/icons-material/SouthEast';
+import NorthEastIcon from '@mui/icons-material/NorthEast';
 import axios from 'axios';
 
 
@@ -21,19 +23,105 @@ const GetEthPrice = () => {
     })
   }, []);
 
+  console.log("geckoAPI:", data);
+
+  // hide component if there's an error with api call
+  if (!data) return null;
+
   return (
     <>
     <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
-      <Typography component="h1" variant="h4" align="center">
+      <Typography component="h1" variant="h4" align="center" gutterBottom>
         Crypto Prices in USD
       </Typography>
 
-      <Grid container spacing={2} >
+      <Grid container spacing={3} >
         <Grid item xs={4}>
+          <Card>
+            <CardMedia
+              component="img"
+              image={data[0].image}
+              sx={{}}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {data[0].name}
+                </Typography>
+                <Typography sx={{ fontSize: 18 }}>
+                  ${data[0].current_price.toLocaleString()}
+                </Typography>
 
+                {data[0].price_change_percentage_24h < 0 ? (
+                  <Typography variant="body2" color="red">
+                    <SouthEastIcon fontSize='small'/>{data[0].price_change_percentage_24h.toFixed(2)}%
+                  </Typography>
+                ) : (
+                  <Typography variant="body2" color="green">
+                    <NorthEastIcon fontSize='small'/>{data[0].price_change_percentage_24h.toFixed(2)}%
+                  </Typography>
+                )}
+
+              </CardContent>
+          </Card>
+        </Grid>
+
+        <Grid item xs={4}>
+          <Card>
+            <CardMedia
+              component="img"
+              image={data[1].image}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {data[1].name}
+                </Typography>
+                <Typography sx={{ fontSize: 18 }}>
+                  ${data[1].current_price.toLocaleString()}
+                </Typography>
+
+                {data[1].price_change_percentage_24h < 0 ? (
+                  <Typography variant="body2" color="red">
+                    <SouthEastIcon fontSize='small'/>{data[1].price_change_percentage_24h.toFixed(2)}%
+                  </Typography>
+                ) : (
+                  <Typography variant="body2" color="green">
+                    <NorthEastIcon fontSize='small'/>{data[1].price_change_percentage_24h.toFixed(2)}%
+                  </Typography>
+                )}
+
+              </CardContent>
+          </Card>
+        </Grid>
+
+        <Grid item xs={4}>
+          <Card>
+            <CardMedia
+              component="img"
+              image={data[2].image}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {data[2].name}
+                </Typography>
+                <Typography sx={{ fontSize: 18 }}>
+                  ${data[2].current_price.toLocaleString()}
+                </Typography>
+
+                {data[2].price_change_percentage_24h < 0 ? (
+                  <Typography variant="body2" color="red">
+                    <SouthEastIcon fontSize='small'/>{data[2].price_change_percentage_24h.toFixed(2)}%
+                  </Typography>
+                ) : (
+                  <Typography variant="body2" color="green">
+                    <NorthEastIcon fontSize='small'/>{data[2].price_change_percentage_24h.toFixed(2)}%
+                  </Typography>
+                )}
+
+              </CardContent>
+          </Card>
         </Grid>
       </Grid>
-      </Paper>
+    </Paper>
     </>
   )
 }
