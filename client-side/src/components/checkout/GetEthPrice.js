3114096a9957dd4a285f429e90c528682a145dda
Winston Limanto <wmlimanto@gmail.com>
2022-10-10 19:00:56 -0700

    remove paper, add solana, set default destination address to admin_coinbarter

--- client-side/src/components/checkout/GetEthPrice.js
+++ client-side/src/components/checkout/GetEthPrice.js
@@ -12,7 +12,7 @@ const GetEthPrice = () => {
 
   const [data, setData] = useState(null);
 
-  const geckoURL = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin%2C%20ethereum%2C%20litecoin&order=market_cap_desc&per_page=3&page=1&sparkline=false'
+  const geckoURL = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin%2C%20ethereum%2C%20litecoin%2C%20solana&order=market_cap_desc&per_page=3&page=1&sparkline=false'
 
   // call crypto price data from coingecko api
   useEffect(() => {
@@ -30,24 +30,20 @@ const GetEthPrice = () => {
 
   return (
     <>
-    <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
-      <Typography component="h1" variant="h4" align="center" gutterBottom>
-        Crypto Prices in USD
-      </Typography>
-
       <Grid container spacing={3} >
-        <Grid item xs={4}>
-          <Card>
+
+        <Grid item xs={3}>
+          <Card sx={{ display: 'flex'}}>
             <CardMedia
               component="img"
               image={data[0].image}
-              sx={{ width: "150px", p: "1rem", mx: 1, mt: 1}}
+              sx={{ width: "80px", height: "100%", mx: 1, mt: 1}}
               />
               <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
+                <Typography gutterBottom variant="h6" component="div">
                   {data[0].name}
                 </Typography>
-                <Typography sx={{ fontSize: 18 }}>
+                <Typography sx={{ fontSize: 15 }}>
                   ${data[0].current_price.toLocaleString()}
                 </Typography>
 
@@ -65,18 +61,18 @@ const GetEthPrice = () => {
           </Card>
         </Grid>
 
-        <Grid item xs={4}>
-          <Card>
+        <Grid item xs={3}>
+          <Card sx={{ display: 'flex'}}>
             <CardMedia
               component="img"
               image={data[1].image}
-              sx={{ width: "150px", p: "1rem", mx: 1, mt: 1}}
+              sx={{ width: "80px", height: "100%", mx: 1, mt: 1}}
               />
               <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
+                <Typography gutterBottom variant="h6" component="div">
                   {data[1].name}
                 </Typography>
-                <Typography sx={{ fontSize: 18 }}>
+                <Typography sx={{ fontSize: 15 }}>
                   ${data[1].current_price.toLocaleString()}
                 </Typography>
 
@@ -94,18 +90,18 @@ const GetEthPrice = () => {
           </Card>
         </Grid>
 
-        <Grid item xs={4}>
-          <Card>
+        <Grid item xs={3}>
+          <Card sx={{ display: 'flex'}}>
             <CardMedia
               component="img"
               image={data[2].image}
-              sx={{ width: "150px", p: "1rem", mx: 1, mt: 1}}
+              sx={{ width: "80px", height: "100%", mx: 1, mt: 1}}
               />
               <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
+                <Typography gutterBottom variant="h6" component="div">
                   {data[2].name}
                 </Typography>
-                <Typography sx={{ fontSize: 18 }}>
+                <Typography sx={{ fontSize: 15 }}>
                   ${data[2].current_price.toLocaleString()}
                 </Typography>
 
@@ -122,8 +118,37 @@ const GetEthPrice = () => {
               </CardContent>
           </Card>
         </Grid>
+
+        <Grid item xs={3}>
+          <Card sx={{ display: 'flex'}}>
+            <CardMedia
+              component="img"
+              image={data[3].image}
+              sx={{ width: "80px", height: "100%", mx: 1, mt: 1}}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h6" component="div">
+                  {data[3].name}
+                </Typography>
+                <Typography sx={{ fontSize: 15 }}>
+                  ${data[3].current_price.toLocaleString()}
+                </Typography>
+
+                {data[3].price_change_percentage_24h < 0 ? (
+                  <Typography variant="body2" color="red">
+                    <SouthEastIcon fontSize='small'/> {data[3].price_change_percentage_24h.toFixed(2)}%
+                  </Typography>
+                ) : (
+                  <Typography variant="body2" color="green">
+                    <NorthEastIcon fontSize='small'/> {data[3].price_change_percentage_24h.toFixed(2)}%
+                  </Typography>
+                )}
+
+              </CardContent>
+          </Card>
+        </Grid>
+
       </Grid>
-    </Paper>
     </>
   )
 }
